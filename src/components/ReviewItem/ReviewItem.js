5e856d4d03458024b7451adac1b1712df0031b49
JannatulHappy <jannatulferdoushappy21@gmail.com>
2021-10-16 20:15:32 +0600

    remove update

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const ReviewItem = (props) => {
-  const {img, name, quantity, price } = props.product;
+  const {img, name, quantity, price,key } = props.product;
+  const {handleRemove}=props
   return (
     <div className="product">
       <div>
@@ -11,7 +12,7 @@ const ReviewItem = (props) => {
         <h3 className="product-name">{name}</h3>
         <h3 className="product-price">price : {price}</h3>
         <p>Quantity : {quantity}</p>
-        <button className="btn-regular">Remove</button>
+        <button onClick={()=>handleRemove(key)} className="btn-regular">Remove</button>
       </div>
     </div>
   );
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -19,6 +19,6 @@ const useCart = (products) => {
       setCart(storedCart);
     }
   }, [products]);
-  return [cart];
+  return [cart,setCart];
 };
 export default useCart;
